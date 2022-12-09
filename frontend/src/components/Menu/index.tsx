f9d5f38317d40fb5e929997d2aea8b5074e85c91
Łukasz Wojdat <sampak.dev@gmail.com>
2022-12-09 10:28:04 +0100

    Add logo to menu

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,10 +1,17 @@
 import styles from './styles.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import Logo from 'components/Logo';
+import { useNavigate } from 'react-router-dom';
 
 const Menu = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.menu}>
+      <div className={styles.logo}>
+        <Logo size={24} />
+      </div>
       <div className={styles.navigation}>
         <div className={`${styles.option} ${styles.active}`}>
           <FontAwesomeIcon icon={faHome} />
@@ -12,7 +19,7 @@ const Menu = () => {
         </div>
         <div className={styles.option}>Features</div>
         <div className={styles.option}>Stats</div>
-        <div className={styles.option}>
+        <div onClick={() => navigate('/auth/signin')} className={styles.option}>
           <FontAwesomeIcon icon={faSignInAlt} />
           Login/Register
         </div>
