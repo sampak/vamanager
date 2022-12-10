7bb2b7f5f45fca0e7b48e3ad2d1f87d0ff0cea84
Łukasz Wojdat <sampak.dev@gmail.com>
2022-12-10 06:46:13 +0100

    fix imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,8 +5,6 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AirportModule } from './airport/airport.module';
 import { AirlineModule } from './airline/airline.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 @Module({
   imports: [AuthModule, UserModule, AirportModule, AirlineModule],
   controllers: [AppController],
