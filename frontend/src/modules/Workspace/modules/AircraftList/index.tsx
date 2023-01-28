8cf0531c3c748f546551527e44af5f7fb5fd24f5
Łukasz Wojdat <sampak.dev@gmail.com>
2023-01-28 22:20:04 +0100

    fix/missing buttons in aircrafts, wrong showing empty state when loading is in progress

--- frontend/src/modules/Workspace/modules/AircraftList/index.tsx
+++ frontend/src/modules/Workspace/modules/AircraftList/index.tsx
@@ -6,24 +6,37 @@ import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import EmptyState from './components/EmptyState';
 import styles from './styles.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { navigateInsideWorkspace } from 'utils/navigateInsideWorkspace';
+import { useNavigate } from 'react-router-dom';
 
 const AircraftList = () => {
   const { workspaceId } = useParams();
+  const navigate = useNavigate();
   const { data, isFetching } = airlineService.useGetAircrafts(workspaceId!);
-
   const aircrafts = useMemo(() => data?.data ?? [], [data]);
   return (
     <div className={styles.container}>
-      <Title black>Company Aircrafts</Title>
-      {isFetching && <Loading />}
-      {!isFetching && !!aircrafts.length ? (
+      <div className={styles.header}>
+        <Title black>Company Aircrafts</Title>
+        <div
+          onClick={() =>
+            navigateInsideWorkspace(navigate, workspaceId, '/aircrafts/dealer')
+          }
+          className={styles.createButton}
+        >
+          <FontAwesomeIcon icon={faCirclePlus} /> Buy Aircraft
+        </div>
+      </div>
+      {isFetching && <Loading className={styles.loading} />}
+      {!isFetching && !aircrafts.length && <EmptyState />}
+      {!isFetching && !!aircrafts.length && (
         <div className={styles.cardsWrapper}>
           {aircrafts.map((aircraft) => (
             <AircraftCard aircraft={aircraft} />
           ))}
         </div>
-      ) : (
-        <EmptyState />
       )}
     </div>
   );
